6cfd451c4cd3d679e49bb99408ecfd96b3a4390f
RKopczynski <kopczak1995@gmail.com>
2017-03-16 14:18:07 +0100

    [Game] New control for editing color of tank

---
+++ TanksOnline.ProjektPZ.Game/Controls/TankPreview.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SFML.Graphics;
+using SFML.Window;
+using System.Windows.Threading;
+using TanksOnline.ProjektPZ.Game.Drawables.Tank;
+using SFML.System;
+
+namespace TanksOnline.ProjektPZ.Game.Controls
+{
+    public partial class TankPreview : UserControl
+    {
+        public byte RED {
+            get { return _red; }
+            set {
+                _colorChanged = true;
+                _red = value;
+            }
+        }
+        public byte GREEN {
+            get { return _green; }
+            set {
+                _colorChanged = true;
+                _green = value;
+            }
+        }
+        public byte BLUE {
+            get { return _blue; }
+            set {
+                _colorChanged = true;
+                _blue = value;
+            }
+        }
+        
+        private RenderWindow _renderWindow;
+        private DispatcherTimer _timer;
+        private Color _tankColor;
+        private bool _colorChanged;
+        private byte _red, _blue, _green;
+        private Tank _tank;
+
+        public TankPreview()
+        {
+            InitializeComponent();
+        }
+
+        public void RunPreview()
+        {
+            var context = new ContextSettings { DepthBits = 24, AntialiasingLevel = 16 };
+            this._renderWindow = new RenderWindow(this.Handle, context);
+            this._renderWindow.SetActive(true);
+
+            this._timer = new DispatcherTimer { Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60) };
+            this._timer.Tick += MainLoop;
+            this._timer.Start();
+
+            this._tankColor = Color.Green;
+            this._colorChanged = false;
+            // NOTE MACIEK: Zmiana wielkości czołgu w konstruktorze, a jego przemieszczanie w position
+            this._tank = new Tank(20f)
+            {
+                FillColor = _tankColor,
+                Position = new Vector2f(40f, 30f),
+                TurretAngle = 90
+            };
+        }
+
+        private void MainLoop(object sender, EventArgs e)
+        {
+            if (_colorChanged)
+            {
+                _tankColor = new Color(_red, _green, _blue);
+                _tank.FillColor = _tankColor;
+                _colorChanged = false;
+            }
+            // NOTE MACIEK: Zmiana koloru tła
+            this._renderWindow.Clear(new Color(50, 50, 50));
+            this._renderWindow.Draw(_tank);
+            this._renderWindow.Display();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (_renderWindow == null) base.OnPaint(e);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            if (_renderWindow == null) base.OnPaintBackground(e);
+        }
+    }
+}
--- TanksOnline.ProjektPZ.Game/Views/TestWindow.cs
+++ TanksOnline.ProjektPZ.Game/Views/TestWindow.cs
@@ -17,69 +17,38 @@ namespace TanksOnline.ProjektPZ.Game.Views
 
     public partial class TestWindow : Form
     {
-        /** 
-            g = 9.81
-            t = 0
-            f - siła oporu powietrza
-            m - masa ciała
-            v - prędkość początkowa
-            Δt - przyrost czasu
-            α - kąt wystrzelenia pocisku
-            kod:
-            x = x + ((v* cos(α)) - f / m * t)
-            y = y - ((v* sin(α)) - g * t)
-            t = t + Δt */
-
-        private RenderWindow _renderWindow;
-        private Vector2f startPos = new Vector2f(400f, 250f);
-        private List<CircleShape> circles = new List<CircleShape>(100);
-        private const float g = 9.81f;
-        private const float m = 10f;
-
-        private DispatcherTimer _timer;
-
         public TestWindow()
         {
             InitializeComponent();
 
-            var context = new ContextSettings { DepthBits = 24, AntialiasingLevel = 16 };
-            this._renderWindow = new RenderWindow(this.sfmlRenderControl1.Handle, context);
-            this._renderWindow.SetActive(true);
-
-            for(int i = 0; i < 100; i++) circles.Add(new CircleShape(1));
-            circles.First().FillColor = Color.Red;
-            UpdateFoo_Click(null, null);
-
-            this._timer = new DispatcherTimer { Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60) };
-            this._timer.Tick += MainLoop;
-            this._timer.Start();
+            this.TankPreview.RunPreview();
         }
 
-        private void UpdateFoo_Click(object sender, EventArgs e)
+        private void Red_TextChanged(object sender, EventArgs e)
         {
-            var t = 0.1f;
-            var velocity = new Vector2f( (float) Math.Cos(45), (float) Math.Sin(45)) * 20;
-            var position = startPos;
-
-            foreach (var circle in circles)
+            try
             {
-                var acc = g / m;
-                velocity += new Vector2f(0, acc * t);
-                position += velocity * t;
-                
-                circle.Position = position;
-                t += 0.1f;
-                Console.WriteLine("Pozycja " + t + ": " + position);
+                this.TankPreview.RED = byte.Parse(Red.Text);
             }
+            catch (FormatException) { }
         }
 
-        private void MainLoop(object sender, EventArgs e)
+        private void Green_TextChanged(object sender, EventArgs e)
         {
-            this._renderWindow.DispatchEvents();
-            this._renderWindow.Clear(new Color(50, 50, 50));
-            this.circles.ForEach(x => this._renderWindow.Draw(x));
+            try
+            {
+                this.TankPreview.GREEN = byte.Parse(Green.Text);
+            }
+            catch (FormatException) { }
+        }
 
-            this._renderWindow.Display();
+        private void Blue_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                this.TankPreview.BLUE = byte.Parse(Blue.Text);
+            }
+            catch (FormatException) { }
         }
     }
 }
