a24ee9fcc5aeed6f0d48b22348f5f8d7685b6d47
RKopczynski <kopczak1995@gmail.com>
2017-04-06 15:27:02 +0200

    DbSeed corrections

--- TanksOnline.ProjektPZ.Server/TanksOnline.ProjektPZ.Server/Migrations/Configuration.cs
+++ TanksOnline.ProjektPZ.Server/TanksOnline.ProjektPZ.Server/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace TanksOnline.ProjektPZ.Server.Migrations
 {
+    using Domain;
     using Domain.Entities;
     using System;
     using System.Collections.Generic;
@@ -29,33 +30,58 @@ namespace TanksOnline.ProjektPZ.Server.Migrations
             //    );
             //
 
-            if (!context.Users.Any())
+            var users = new List<User>();
+            for (int i = 1; i < 10; i++)
             {
-                var users = new List<User>();
-                for (int i = 0; i < 5; i++)
+                users.Add(new User
                 {
-                    users.Add(new User
-                    {
-                        Name = $"test{i}",
-                        Email = $"test@test{i}.pl",
-                        Password = "test",
-                        TankInfo = new TankInfo { Name = "test", ColorR = 100, ColorG = 100, ColorB = 100 },
-                        UserScore = new UserScore(),
-                    });
-                }
-
-                context.Users.AddRange(new List<User>(users)
-                {
-                    new User
-                    {
-                        Name = "test",
-                        Email = "test",
-                        Password = "test",
-                        TankInfo = new TankInfo { Name = "test", ColorR = 100, ColorG = 100, ColorB = 100 },
-                        UserScore = new UserScore(),
-                    }
+                    Id = i,
+                    Name = $"test{i}",
+                    Email = $"test{i}@test.pl",
+                    Password = "test",
+                    TankInfo = new TankInfo { Name = $"Tester0{i}", ColorR = 100, ColorG = 100, ColorB = 100 },
+                    UserScore = new UserScore(),
+                    Status = Domain.Enums.UserStatus.Offline,
                 });
             }
+            context.Users.AddOrUpdate(users.ToArray());
+            context.SaveChanges();
+
+            context.Players.AddOrUpdate(new Player[2]
+            {
+                new Player
+                {
+                    Id = 1,
+                    IdInMatch = 0,
+                    TankHP = Consts.DEFAULT_TANK_HP,
+                    TurretAngle = Consts.DEFAULT_TURRET_ANGLE,
+                    User = context.Users.Find(1),
+                },
+                new Player {
+                    Id = 2,
+                    IdInMatch = 1,
+                    TankHP = Consts.DEFAULT_TANK_HP,
+                    TurretAngle = Consts.DEFAULT_TURRET_ANGLE,
+                    User = context.Users.Find(2),
+                },
+            });
+            context.SaveChanges();
+
+            var players = context.Players.Take(2).ToList();
+            context.GameRooms.AddOrUpdate(new GameRoom
+            {
+                Id = 1,
+                Match = new Match
+                {
+                    ActualPlayer = 0,
+                    Players = players
+                },
+                Owner = context.Users.Find(1),
+                PlayersLimit = 2,
+                RoomStatus = Domain.Enums.RoomStatus.Ready,
+                Players = players,
+            });
+            context.SaveChanges();
         }
     }
 }
