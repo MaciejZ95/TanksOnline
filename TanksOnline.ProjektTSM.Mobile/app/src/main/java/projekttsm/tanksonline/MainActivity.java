a4e8e25b80dd2e3a659ba8254eb69e222d2eba73
RKopczynski <kopczak1995@gmail.com>
2017-05-08 22:55:10 +0200

    Push simple mobile RESTful connection

--- TanksOnline.ProjektTSM.Mobile/app/src/main/java/projekttsm/tanksonline/MainActivity.java
+++ TanksOnline.ProjektTSM.Mobile/app/src/main/java/projekttsm/tanksonline/MainActivity.java
@@ -5,13 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+
+import org.json.JSONObject;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -31,9 +39,32 @@ public class MainActivity extends AppCompatActivity {
      * to duży baner
      */
 
+    String url = "http://192.168.0.19/";
+
+    JsonObjectRequest jsObjRequest = new JsonObjectRequest
+            (Request.Method.GET, url + "api/gamerooms/1014", null, new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.e("TESTY", "O, wyświetliło się");
+                    ((TextView)findViewById(R.id.helloWorld)).setText("Response " + response.toString());
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // TODO Auto-generated method stub
+                    Log.e("ERROR", "NIe działa tak bardzo");
+                    Log.e("ERROR", error.toString());
+                }
+            });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
